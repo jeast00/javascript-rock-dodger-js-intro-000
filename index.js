d2bc02bf46edbfd66878462bb26d5c16a83501eb
Jonathan East <jonathan_east@ymail.com>
2020-03-26 17:22:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ function createRock(x) {
      * But if the rock *has* reached the bottom of the GAME,
      * we should remove the rock from the DOM.
      */
-     rock.style.top = `${top += 2}px`
+     rock.style.top = `${topRock += 2}px`
      if(top < 300) {
        window.requestAnimationFrame(moveRock)
      }else {
