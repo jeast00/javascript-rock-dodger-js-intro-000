fb1274a41420e6ef13b4f3165261bba2ffd61ab6
Jonathan East <jonathan_east@ymail.com>
2020-03-26 16:06:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -138,8 +138,12 @@ function moveDodger(e) {
    */
    if(e.which == LEFT_ARROW) {
      moveDodgerLeft()
+     e.preventDefault()
+     e.stopPropagation()
    } else if (e.which == RIGHT_ARROW) {
      moveDodgerRight()
+     e.preventDefault()
+     e.stopPropagation()
    } else {
      return false
    }
